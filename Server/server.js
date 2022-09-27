86ffa65358d79fbda383cefa92d8915594bdc250
Sam Phipps <samuel.v.phipps@gmail.com>
2022-09-27 16:43:58 -0500

    class done

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,10 @@
 //this is the file we are gonna run
-
+let comments= [
+    'SERVERS ARE GREAT',
+    'You suck',
+    'I want awards',
+    'fancy'
+];
 const express= require('express');
 
 //express is a function
@@ -8,7 +13,22 @@ const app=express();
                     //string is file path to public folder
 app.use(express.static('./server/public'));    
 
-   //first argument is a number, second is a function
+//App get is an event handler for I WANT COMMENTS    
+//event app.on(get)
+                    //request and response
+app.get('/comments', (req, res) => {
+    console.log('jimmy wants those comments');
+
+    res.send(comments);
+});
+
+//first comment
+
+app.get('/comments/first', (req, res)=>{
+    console.log('comments/first');
+    res.send(comments[0]);
+})
+   //first argument is the port number, second is a function
         //function serves as the onready function
         //use over 3000 for these
         // we are 'listening' on port 3000
